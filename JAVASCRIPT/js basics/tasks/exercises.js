dc165978a0458d312c8e8a12ab5e763bba5baab7
AndreaGarzoglio <77269125+AndreaGarzoglio@users.noreply.github.com>
2025-10-08 18:09:38 +0200

    Completed exercises

--- JAVASCRIPT/js basics/tasks/exercises.js
+++ JAVASCRIPT/js basics/tasks/exercises.js
@@ -95,5 +95,20 @@ let strings = ["Hare", "Krishna", "Hare", "Krishna",
   "Krishna", "Krishna", "Hare", "Hare", ":-O"
 ];
 
-alert( unique(strings) ); // Hare, Krishna, :-O
+alert( unique(strings) );( // Hare, Krishna, :-O
 */
+
+function groupById(arr){
+  return arr.reduce((obj, value) => {
+    obj[value.id]= value;
+    return obj;
+  }, {})
+}
+
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+let usersById = groupById(users);
